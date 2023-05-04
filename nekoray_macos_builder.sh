44cfce4712b449336a83ae6a56c9bc7b550d295d
livenux <livenux@foxmail.com>
2023-05-04 17:53:15 +0800

    fix directory error.

--- nekoray_macos_builder.sh
+++ nekoray_macos_builder.sh
@@ -70,14 +70,15 @@ fi
 
 # Get and Build dependencies
 bash build_deps_all.sh
-# get go build dependencies
-bash libs/get_source.sh
 
 cd "$nPath/build"
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DNKR_PACKAGE_MACOS=1 ..
 ninja
 
 cd $nPath
+
+# get go build dependencies
+bash libs/get_source.sh
 
 nApp=$nPath/build/nekoray.app
 
